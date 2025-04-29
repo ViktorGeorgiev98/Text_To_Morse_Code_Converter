b70eabb1dfea765d8a8fde45d8f47041e3ced0f1
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2025-04-29 21:06:05 +0300

    created dictionaries for morse code and started working on the converter class

---
+++ morse_code_converter.py
@@ -0,0 +1,48 @@
+# dictionaries for morse code conversion
+
+# text to morse code
+text_to_morse = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+
+    # Numbers
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', 
+    '4': '....-', '5': '.....', 6: '-....', 7: '--...', 8: "---..", 9: "----.",
+
+    # Punctuation
+    ', ': '--..--', '. ': '.-.-.-', '? ': '..--..', '/ ': '-..-.',
+    '- ': '-....-', '(' : '-.--.', ') ': '-.--.-'
+}
+
+# morse code to text 
+morse_to_text = {value: key for key, value in text_to_morse.items()}
+
+
+
+class Morse_Code_Converter:
+    def __init__(self):
+        self.text_to_morse = text_to_morse
+        self.morse_to_text = morse_to_text
+        
+        
+    def convert_text_to_morse(self, text):
+        """Convert text to morse code"""
+        converted_text = ""
+        for char in text:
+            if char.upper() in self.text_to_morse:
+                converted_text += self.text_to_morse[char.upper()]
+            
+            if char == " ":
+                converted_text += " / " # space in morse code
+        
+        return converted_text
+    
+    def convert_morse_to_text(self, morse_code):
+        """Convert morse code to text"""
+        words = morse_code.split(" / ")
+        decoded_text = ""
+        for char in words:
+            
